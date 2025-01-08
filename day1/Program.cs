e4b34765ac66ec32448021e2e941a18963efc2f5
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-01-08 14:02:10 -0500

    sort sequences and display the result of CompareColumns

--- day1/Program.cs
+++ day1/Program.cs
@@ -4,3 +4,7 @@ List<string> part1InputList = Part1.ReadInput();
 List<string>[] comparisonSequences = Part1.SplitLines(part1InputList);
 List<int[]> leftColumnIntegers = Part1.SplitSequences(comparisonSequences[0]);
 List<int[]> rightColumnIntegers = Part1.SplitSequences(comparisonSequences[1]);
+List<int[]> leftColumnSortedIntegers = Part1.SortSequences(leftColumnIntegers);
+List<int[]> rightColumnSortedIntegers = Part1.SortSequences(rightColumnIntegers);
+
+Console.WriteLine(Part1.CompareColumns(leftColumnSortedIntegers, rightColumnSortedIntegers));
