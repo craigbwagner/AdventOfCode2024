ec34363403a30cc874fb33c0af22bd1eb62c1eec
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-01-08 14:01:40 -0500

    split sequences, sort those sequences using bubble sort, and sum up the total differences between the two columns

--- day1/Part1/Part1.cs
+++ day1/Part1/Part1.cs
@@ -2,7 +2,7 @@ namespace day1.Part1;
 
 public class Part1
 {
-    public List<string> readInput()
+    public static List<string> ReadInput()
     {
         List<string> inputLines = [];
         try
@@ -22,7 +22,7 @@ public class Part1
         }
         return inputLines;
     }
-    public List<string>[] splitLines(List<string> input)
+    public static List<string>[] SplitLines(List<string> input)
     {
         List<string> list1 = [];
         List<string> list2 = [];
@@ -34,5 +34,74 @@ public class Part1
             list2.Add(sequences[1]);
         }
         return [list1, list2];
+    }
+    public static List<int[]> SplitSequences(List<string> input)
+    {
+        List<int[]> arrayOfDigits = [];
+
+        foreach (string sequence in input)
+        {
+            char[] digitsAsChars = sequence.ToCharArray();
+            int[] digitsAsInts = Array.ConvertAll<char, int>(digitsAsChars, c => (int)char.GetNumericValue(c));
+            arrayOfDigits.Add(digitsAsInts);
+        }
+        // foreach (var item in arrayOfDigits)
+        // {
+        //     foreach (var n in item)
+        //     {
+        //         Console.WriteLine(n);
+        //     }
+        // }
+        return arrayOfDigits;
+    }
+    public static List<int[]> SortSequences(List<int[]> unsortedColumnSequences)
+    {
+        List<int[]> sortedColumnSequences = [];
+        int i, temp, swapCounter;
+        bool swapped;
+
+        foreach (int[] sequence in unsortedColumnSequences)
+        {
+            swapped = true;
+            swapCounter = 0;
+            while (swapped == true)
+            {
+                for (i = 0; i < 4; i++)
+                {
+                    if (sequence[i] > sequence[i + 1])
+                    {
+                        swapCounter++;
+                        temp = sequence[i];
+                        sequence[i] = sequence[i + 1];
+                        sequence[i + 1] = temp;
+                    }
+                }
+
+                if (swapCounter == 0)
+                {
+                    swapped = false;
+                }
+                else
+                {
+                    swapCounter = 0;
+                }
+            }
+            sortedColumnSequences.Add(sequence);
+        }
+
+        return sortedColumnSequences;
+    }
+
+    public static int CompareColumns(List<int[]> leftColumnSorted, List<int[]> rightColumnSorted)
+    {
+        int totalDifference = 0;
+        for (int i = 0; i < leftColumnSorted.Count && i < rightColumnSorted.Count; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                totalDifference += Math.Abs(leftColumnSorted[i][j] - rightColumnSorted[i][j]);
+            }
+        }
+        return totalDifference;
     }
 }
